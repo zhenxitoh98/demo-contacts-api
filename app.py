e8e312198dc4e321bc8860c38c94de345fdaf381
Samuel Toh <zhenxitoh@gmail.com>
2020-09-28 11:54:30 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,143 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, make_response, request
+from flask_restful import Api, reqparse, Resource
+from base import People, db
+
+app = Flask(__name__)
+api = Api(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+db.init_app(app)
+app.app_context().push()
+db.create_all()
+
+# To GET or POST name, height, and age from the database
+class People_List(Resource):
+    # Get details from the json data
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, required=True, help='Name of the person')
+    parser.add_argument('height', type=int, required=True, help='Height of the person')
+    parser.add_argument('age', type=int, required=True, help='Age of the person')
+
+    # Return details from the database
+    def get(self, id):
+        item = People.find_by_id(id)
+        if item:
+            return item.json()
+        return {'Message': 'The person is not found'}
+
+    # Add details to the database
+    def post(self, id):
+        if People.find_by_id(id) :
+            return {'Message': 'Person with the id {} already exists'.format(id)}
+
+        args = People_List.parser.parse_args()
+        item = People(id, args['name'], args['height'], args['age'], None, None)
+
+        item.save_to()
+        return item.json()
+
+    # Update the database, if needed
+    def put(self, id):
+        args = People_List.parser.parse_args()
+        item = People.find_by_id(id)
+        if item:
+            item.name = args['name']
+            item.height = args['height']
+            item.age = args['age']
+            item.save_to()
+            return {'name':args['name'], 'height':args['height'], 'age':args['age']}
+        item = People(id, args['name'], args['height'], args['age'], None, None)
+        item.save_to()
+        return item.json()
+
+    # Delete the database, if needed
+    def delete(self, id):
+        item  = People.find_by_id(id)
+        if item:
+            item.delete_()
+            return {'Message': '{} has been deleted from records'.format(id)}
+        return {'Message': '{} is already not on the list'.format()}
+
+# To GET or POST email, and number from the database
+class Contact_List(Resource):
+    # Get details from the json data
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', type=str, required=True, help='Email of the person')
+    parser.add_argument('number', type=str, required=True, help='Number of the person')
+
+    # Return details from the database
+    def get(self, id):
+        item = People.find_by_id(id)
+        if item:
+            return item.json2()
+        return {'Message': 'The person is not found'}
+
+    # Add details to the database
+    def post(self, id):
+        args = Contact_List.parser.parse_args()
+        item = People.find_by_id(id)
+        if item:
+            item.email = args['email']
+            item.number = args['number']
+            item.save_to()
+            return {'email':args['email'], 'number':args['number']}
+        item = People(id, item.name, item.height, item.age, args['email'], args['number'])
+        item.save_to()
+        return item.json2()
+
+    # Delete the database, if needed
+    def delete(self, id):
+        item  = People.find_by_id(id)
+        if item:
+            item.delete_()
+            return {'Message': '{} has been deleted from records'.format(id)}
+        return {'Message': '{} is already not on the list'.format()}
+
+# Return name, age, and height of everyone
+class All_People(Resource):
+    def get(self):
+        return {'Persons': list(map(lambda x: x.json(), People.query.all()))}
+
+# Return email and number of everyone
+class All_Contact(Resource):
+    def get(self):
+        return {'Contact': list(map(lambda x: x.json2(), People.query.all()))}
+
+# To get the details of a person based on the query parameters (name, email, number)
+@app.route('/contacts/', methods=["GET"])
+def api_filter():
+    query_parameters = request.args
+
+    name = query_parameters.get('name')
+    email = query_parameters.get('email')
+    number = query_parameters.get('number')
+
+    item = None
+    if name:
+        item = People.query.filter_by(name=name).first()
+    elif email:
+        item = People.query.filter_by(email=email).first()
+    elif number:
+        item = People.query.filter_by(number=number).first()
+
+    if item == None:
+        return not_found(404)
+
+    return item.jsonAll()
+
+api.add_resource(All_People, '/all_people/')
+api.add_resource(People_List, '/people/<int:id>/')
+api.add_resource(All_Contact, '/all_contacts/')
+api.add_resource(Contact_List, '/people/<int:id>/contacts/')
+
+# when no pages are found or there is error
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ base.py
@@ -0,0 +1,44 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# The database
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=False, nullable=False)
+    height = db.Column(db.Integer, unique=False, nullable=False)
+    age = db.Column(db.Integer, unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
+    number = db.Column(db.String(120), unique=True, nullable=True)
+
+    def __init__(self, id, name, height, age, email, number):
+        self.id = id
+        self.name = name
+        self.age = age
+        self.height = height
+        self.email = email
+        self.number = number
+
+    def json(self):
+        return {'ID': self.id, 'Name': self.name, 'Age': self.age, 'Height': self.height}
+
+    def json2(self):
+        return {'ID': self.id, 'Email': self.email, 'Number': self.number}
+
+    def jsonAll(self):
+        return {'ID': self.id, 'Name': self.name, 'Age': self.age, 'Height': self.height, 'Email': self.email, 'Number': self.number}
+
+    # check if the user is in the database already
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id = id).first()
+
+    # update the database
+    def save_to(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # delete the database
+    def delete_(self):
+        db.session.delete(self)
+        db.session.commit()
